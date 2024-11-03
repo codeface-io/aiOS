b5f7c5e1d83056ace6186a08970e59136fb35668
Sebastian Fichtner <sebastian.fichtner@zuehlke.com>
2024-11-03 01:08:35 +0100

    Add Xcode project

---
+++ Xcode Project/aiOS/aiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  aiOSApp.swift
+//  aiOS
+//
+//  Created by Sebastian Fichtner on 03.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct aiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Xcode Project/aiOSTests/aiOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  aiOSTests.swift
+//  aiOSTests
+//
+//  Created by Sebastian Fichtner on 03.11.2024.
+//
+
+import Testing
+
+struct aiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
