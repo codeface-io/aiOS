8bececc16aab98774759dba2f7414916e2cb7c86
Sebastian Fichtner <sebastian.fichtner@zuehlke.com>
2024-11-08 22:22:52 +0100

    Model how api keys are stored

--- Xcode Project/aiOS/Code/SettingsView.swift
+++ Xcode Project/aiOS/Code/SettingsView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Foundation
 
 struct SettingsView: View {
     var body: some View {
@@ -21,4 +22,23 @@ struct SettingsView: View {
     }
     
     @Environment(\.dismiss) private var dismiss
+}
+
+struct AuthenticationKeyEntry: Codable, Identifiable {
+    init(_ keyValue: String,
+         providerIdentifierValue: String? = nil,
+         name: String? = nil,
+         description: String? = nil) {
+        self.id = UUID()
+        self.keyValue = keyValue
+        self.providerIdentifierValue = providerIdentifierValue
+        self.name = name
+        self.description = description
+    }
+    
+    let id: UUID // Unique identifier for each key entry
+    let keyValue: String
+    let providerIdentifierValue: String? // Optional provider identifier
+    let name: String? // Optional user-given name
+    let description: String? // Optional user-provided description
 }
