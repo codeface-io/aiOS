5d20ea05552a04e3a369965ac4ced09f62abf2ca
Sebastian Fichtner <sebastian.fichtner@zuehlke.com>
2024-11-09 14:57:23 +0100

    Rename an argument

--- Xcode Project/aiOS/Code/Package Candidates/API.Key.swift
+++ Xcode Project/aiOS/Code/Package Candidates/API.Key.swift
@@ -3,14 +3,14 @@ import Foundation
 enum API {
     /// a flexible and future proof way to configure and store api keys. a client does not need to make use of the flexibility and can for instance just assume one key per API without name and description
     struct Key: Codable, Identifiable {
-        init?(_ keyValue: String,
+        init?(_ value: String,
              apiIdentifierValue: String,
              name: String? = nil,
              description: String? = nil,
              id: UUID = UUID()) {
-            if keyValue.isEmpty { return nil }
+            if value.isEmpty { return nil }
             self.id = id
-            self.value = keyValue
+            self.value = value
             self.apiIdentifierValue = apiIdentifierValue
             self.name = name
             self.description = description
