c2242865e402b2ea5c5b5d8fcb73a85a84397901
Sebastian Fichtner <sebastian.fichtner@zuehlke.com>
2024-11-13 02:55:32 +0100

    Visually distinguish user messages more from assisstant messages

--- Xcode Project/aiOS/Code/Chat/MessageView.swift
+++ Xcode Project/aiOS/Code/Chat/MessageView.swift
@@ -32,16 +32,16 @@ struct MessageView: View {
             if isUser { Spacer() }
             
             Text(message.content)
+                .foregroundStyle(isUser ? .secondary : .primary)
                 .lineLimit(nil)
                 .padding()
-                .background(Color.dynamic(.aiOSLevel3))
+                .background(Color.dynamic(isUser ? .aiOSLevel2 : .aiOSLevel3))
                 .clipShape(RoundedRectangle(cornerRadius: 10))
             
             if !isUser { Spacer() }
         }
         .padding(.leading, isUser ? 20 : 0)
         .padding(.trailing, isUser ? 0 : 20)
-        .foregroundStyle(isUser ? .secondary : .primary)
         // the id must be inside here and not outside in the List view so that programmatic scrolling works on macOS. this might actually be a SwiftUI bug.
         .id(message.id)
     }
